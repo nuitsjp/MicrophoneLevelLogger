9a9c95dd2c6c611b3b1d0293c16d2bee504c7f3c
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2022-12-17 06:31:30 +0900

    CommandInvokerでコマンド選択前にマイク一覧を表示するように変更

--- MicrophoneLevelLogger/Command/CommandInvoker.cs
+++ MicrophoneLevelLogger/Command/CommandInvoker.cs
@@ -1,9 +1,11 @@
-﻿using MicrophoneLevelLogger.View;
+﻿using MicrophoneLevelLogger.Domain;
+using MicrophoneLevelLogger.View;
 
 namespace MicrophoneLevelLogger.Command;
 
 public class CommandInvoker : ICommandInvoker
 {
+    private readonly IMicrophonesProvider _microphonesProvider;
     private readonly ICommandInvokerView _view;
     private readonly CalibrateCommand _calibrateCommand;
     private readonly RecordCommand _recordCommand;
@@ -13,18 +15,23 @@ public class CommandInvoker : ICommandInvoker
         ICommandInvokerView view, 
         CalibrateCommand calibrateCommand, 
         RecordCommand recordCommand, 
-        SetMaxInputLevelCommand setMaxInputLevelCommand)
+        SetMaxInputLevelCommand setMaxInputLevelCommand, 
+        IMicrophonesProvider microphonesProvider)
     {
         _view = view;
         _calibrateCommand = calibrateCommand;
         _recordCommand = recordCommand;
         _setMaxInputLevelCommand = setMaxInputLevelCommand;
+        _microphonesProvider = microphonesProvider;
     }
 
     public async Task InvokeAsync()
     {
+        var microphones = _microphonesProvider.Resolve();
         while (true)
         {
+            _view.NotifyMicrophonesInformation(microphones);
+
             var commands = new ICommand[]
             {
                 _calibrateCommand,
--- MicrophoneLevelLogger/View/ICommandInvokerView.cs
+++ MicrophoneLevelLogger/View/ICommandInvokerView.cs
@@ -1,19 +1,8 @@
 ﻿using MicrophoneLevelLogger.Domain;
-using Sharprompt;
 
 namespace MicrophoneLevelLogger.View;
 
-public interface ICommandInvokerView
+public interface ICommandInvokerView : IMicrophoneView
 {
     string SelectCommand(IEnumerable<string> commands);
-}
-
-public class CommandInvokerView : ICommandInvokerView
-{
-    public string SelectCommand(IEnumerable<string> commands)
-    {
-        return Prompt.Select(
-            "コマンドを選択してください。",
-            commands);
-    }
 }
