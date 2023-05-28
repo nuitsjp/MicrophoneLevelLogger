a60473ba44a2ff6e5b276844a40669b9c1fcdf56
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-05-29 04:38:07 +0900

    DecibelConverterの追加

--- Quietrum/Quietrum.ViewModel/MicrophoneViewModel.cs
+++ Quietrum/Quietrum.ViewModel/MicrophoneViewModel.cs
@@ -69,10 +69,11 @@ public partial class MicrophoneViewModel : ObservableObject, IDisposable
             _recordingConfig.RefreshRate);
         var normalize = new Normalize(_recordingConfig.WaveFormat);
         var fastTimeWeighting = new FastTimeWeighting(_recordingConfig.WaveFormat);
-        
+        var converter = new DecibelConverter();
         _disposable = _bufferedObservable
             .Select(normalize.Filter)
             .Select(fastTimeWeighting.Filter)
+            .Select(converter.Convert)
             .Subscribe(OnNext);
     }
 
@@ -106,70 +107,11 @@ public partial class MicrophoneViewModel : ObservableObject, IDisposable
         _waveRecorder = null;
     }
 
-    private List<double> _buffer = new();
-
-    /// <summary>
-    /// RMS（Root Mean Square）を用いて、サンプリング間隔ごとのデシベル値を用いる場合
-    /// </summary>
-    /// <param name="samples"></param>
-    // private void OnNext(WaveInEventArgs e)
-    // {
-    //     // "scroll" the whole chart to the left
-    //     Array.Copy(LiveData, 1, LiveData, 0, LiveData.Length - 1);
-    //
-    //     int bytesPerSample = 2; // 16 bit samples
-    //     int numSamples = e.BytesRecorded / bytesPerSample;
-    //
-    //     double rms = 0;
-    //
-    //     for (int i = 0; i < numSamples; i++)
-    //     {
-    //         short sample = BitConverter.ToInt16(e.Buffer, i * bytesPerSample);
-    //         rms += sample * sample;
-    //     }
-    //
-    //     rms = Math.Sqrt(rms / numSamples);
-    //     double rmsNormalized = rms / 32768.0; // normalize to 0-1 range
-    //
-    //     double decibel = 20 * Math.Log10(rmsNormalized);
-    //
-    //     // Prevent NaN due to log10(0)
-    //     if (double.IsNaN(decibel))
-    //     {
-    //         decibel = Decibel.Minimum.AsPrimitive();
-    //     }
-    //     
-    //     // place the newest data point at the end
-    //     LiveData[^1] = decibel;
-    // }
-
-    private void OnNext(double[] samples)
+    private void OnNext(double decibel)
     {
         // "scroll" the whole chart to the left
         Array.Copy(LiveData, 1, LiveData, 0, LiveData.Length - 1);
-
-        double rms = 0;
-
-        foreach (var sample in samples)
-        {
-            rms += sample * sample;
-        }
-    
-        rms = Math.Sqrt(rms / samples.Length);
-
-        double decibel = 20 * Math.Log10(rms);
-    
-        // Prevent NaN due to log10(0)
-        if (double.IsNaN(decibel) 
-            || double.IsNegativeInfinity(decibel)
-            || decibel < Decibel.MinimumValue)
-        {
-            decibel = Decibel.MinimumValue;
-        }
-        
-        // place the newest data point at the end
         LiveData[^1] = decibel;
-        // _buffer.AddRange(decibels);
         // Minus30dB = $"{_buffer.Count(x => -30d < x) / (double)_buffer.Count * 100d:#0.00}%";
         // Minus40dB = $"{_buffer.Count(x => -40d < x) / (double)_buffer.Count * 100d:#0.00}%";
         // Minus50dB = $"{_buffer.Count(x => -50d < x) / (double)_buffer.Count * 100d:#0.00}%";
---
+++ Quietrum/Quietrum/DecibelConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace Quietrum;
+
+public class DecibelConverter
+{
+    public double Convert(double[] samples)
+    {
+        double rms = 0;
+
+        foreach (var sample in samples)
+        {
+            rms += sample * sample;
+        }
+    
+        rms = Math.Sqrt(rms / samples.Length);
+
+        double decibel = 20 * Math.Log10(rms);
+    
+        // Prevent NaN due to log10(0)
+        if (double.IsNaN(decibel) 
+            || double.IsNegativeInfinity(decibel)
+            || decibel < Decibel.MinimumValue)
+        {
+            decibel = Decibel.MinimumValue;
+        }
+
+        return decibel;
+    }
+
+}
