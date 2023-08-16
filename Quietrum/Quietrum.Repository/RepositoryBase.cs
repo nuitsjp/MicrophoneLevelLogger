337aa2bfc791e113c7c49d4b92b8216e16f8d328
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-08-16 09:41:41 +0900

    ファイルアクセスのロック処理を追加

---
+++ Quietrum/Quietrum.Repository/RepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace Quietrum.Repository;
+
+public abstract class RepositoryBase<T> where T : class
+{
+    private static readonly AsyncLock Lock = new();
+    protected async Task<T> LoadAsync(FileInfo fileInfo, Func<T> getDefault)
+    {
+        using (await Lock.LockAsync())
+        {
+            if (fileInfo.Exists is false)
+            {
+                await SaveAsync(fileInfo, getDefault());
+            }
+
+            await using var stream = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read);
+            return (await JsonSerializer.DeserializeAsync<T>(stream, JsonEnvironments.Options))!;
+        }
+    }
+    
+    protected async Task SaveAsync(FileInfo fileInfo, T value)
+    {
+        using (await Lock.LockAsync())
+        {
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+
+            await using var stream = new FileStream(fileInfo.FullName, FileMode.Create, FileAccess.Write);
+            await JsonSerializer.SerializeAsync(stream, value, JsonEnvironments.Options);
+        }
+    }
+}
--- Quietrum/Quietrum.Repository/SettingsRepository.cs
+++ Quietrum/Quietrum.Repository/SettingsRepository.cs
@@ -1,59 +1,35 @@
-﻿using System.Text.Json;
+﻿using CommunityToolkit.Mvvm.ComponentModel.__Internals;
 
 namespace Quietrum.Repository;
 
 /// <summary>
 /// Settingsのリポジトリー
 /// </summary>
-public class SettingsRepository : ISettingsRepository
+public class SettingsRepository : RepositoryBase<Settings>, ISettingsRepository
 {
-    private const string FileName = $"{nameof(Settings)}.json";
-
-    /// <summary>
-    /// 外部から設定ファイルを更新されることは想定しないため、インスタンスをキャッシュする。
-    /// 主にファイルの同時アクセス制御を簡素化することが目的。
-    /// </summary>
-    private static Settings? _settings;
+    private static readonly FileInfo FileInfo = new($"{nameof(Settings)}.json");
 
     /// <summary>
     /// Settingsをロードする。
     /// </summary>
     /// <returns></returns>
-    public async Task<Settings> LoadAsync()
-    {
-        if (_settings is not null) return _settings;
-        
-        if (File.Exists(FileName) is false)
-        {
-            await SaveAsync(new Settings(
-                "localhost",
-                "localhost",
-                TimeSpan.FromSeconds(30),
-                false,
-                false,
-                null,
-                new List<MicrophoneConfig>()));
-        }
-
-        await using var stream = new FileStream(FileName, FileMode.Open, FileAccess.Read);
-        _settings = (await JsonSerializer.DeserializeAsync<Settings>(stream, JsonEnvironments.Options))!;
-        return _settings;
-    }
+    public Task<Settings> LoadAsync() =>
+        LoadAsync(
+            FileInfo,
+            () =>
+                new Settings(
+                    "localhost",
+                    "localhost",
+                    TimeSpan.FromSeconds(30),
+                    false,
+                    false,
+                    null,
+                    new List<MicrophoneConfig>()));
 
     /// <summary>
     /// Settingsを保存する。
     /// </summary>
     /// <param name="settings"></param>
     /// <returns></returns>
-    public async Task SaveAsync(Settings settings)
-    {
-        if (File.Exists(FileName))
-        {
-            File.Delete(FileName);
-        }
-
-        _settings = settings;
-        await using var stream = new FileStream(FileName, FileMode.Create, FileAccess.Write);
-        await JsonSerializer.SerializeAsync(stream, settings, JsonEnvironments.Options);
-    }
+    public Task SaveAsync(Settings settings) => SaveAsync(FileInfo, settings);
 }
