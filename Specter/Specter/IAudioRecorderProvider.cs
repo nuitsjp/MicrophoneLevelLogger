44060d6ea32a5330ca6711a4d6624f6322d3bd69
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-09-06 11:09:07 +0900

    IAudioInterfaceの追加

---
+++ Specter/Specter/IAudioRecorderProvider.cs
@@ -0,0 +1,12 @@
+﻿using NAudio.Wave;
+
+namespace Specter.Business;
+
+public interface IAudioRecorderProvider
+{
+    IAudioRecorder Resolve(
+        IDevice targetDevice, 
+        Direction direction, 
+        IEnumerable<IDevice> monitoringDevices,
+        WaveFormat waveFormat);
+}
